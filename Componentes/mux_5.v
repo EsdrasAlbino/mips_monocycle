b48c00aa1571218b50713f9013f200a8ddca9306
Lima Araújo <158223826+limaraujo@users.noreply.github.com>
2024-08-05 00:06:25 -0300

    Update mux_5.v

--- Componentes/mux_5.v
+++ Componentes/mux_5.v
@@ -2,8 +2,8 @@ module Mux5_PC (
   input  [31:0] pcPlus4, 
   input  [31:0] Branch, 
   input         PCSrc, 
-  output [31:0] PCin 
+  output [31:0] PCoutMux 
 );
 
-  assign PCin = (PCSrc) ? Branch : pcPlus4;
+  assign PCoutMux = (PCSrc) ? Branch : pcPlus4;
 endmodule
